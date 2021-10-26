cc011f6cba565a9a5aec3e8ad8b7648b458a3555
Anders Hald <chald80@gmail.com>
2021-10-26 20:36:44 +0200

    Add project files.

---
+++ Controllers/HopsController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HopClassLib;
+using HopWebApp.Managers;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HopWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HopsController : ControllerBase
+    {
+        private readonly HopManager _manager = new HopManager();
+
+        
+        // GET: api/<HopsController>
+        [HttpGet]
+        public IEnumerable<Hop> GetAll()
+        {
+            return _manager.GetAll();
+        }
+
+        // GET api/<HopsController>/5
+        [HttpGet("{id}")]
+        public Hop Get(int id)
+        {
+            return _manager.GetById(id);
+        }
+
+        // POST api/<HopsController>
+        [HttpPost]
+        public Hop Post([FromBody] Hop value)
+        {
+            return _manager.Add(value);
+        }
+
+
+        // DELETE api/<HopsController>/5
+        [HttpDelete("{id}")]
+        public Hop Delete(int id)
+        {
+            return _manager.Delete(id);
+        }
+    }
+}
---
+++ Managers/HopManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HopClassLib;
+
+namespace HopWebApp.Managers
+{
+    public class HopManager
+        {
+            private static int _nextId = 1;
+
+            private static readonly List<Hop> Data = new List<Hop>
+            {
+                new Hop {Id = _nextId++, Name = "1", HarvestYear = 1, Price = 1, AlphaAcid = 1},
+                new Hop {Id = _nextId++, Name = "2", HarvestYear = 2, Price = 2, AlphaAcid = 2},
+                new Hop {Id = _nextId++, Name = "3", HarvestYear = 3, Price = 3, AlphaAcid = 3}
+            };
+
+            public List<Hop> GetAll()
+            {
+                return new List<Hop>(Data);
+            }
+
+            public Hop GetById(int id)
+            {
+                return Data.Find(h => h.Id == id);
+            }
+
+            public Hop Add(Hop newHop)
+            {
+                newHop.Id = _nextId;
+                Data.Add(newHop);
+                return newHop;
+            }
+
+            public Hop Delete(int id)
+            {
+                Hop hop = Data.Find(h => h.Id == id);
+                if (hop == null) return null;
+                Data.Remove(hop);
+                return hop;
+                {
+                    
+                }
+            }
+
+        }
+    
+}
